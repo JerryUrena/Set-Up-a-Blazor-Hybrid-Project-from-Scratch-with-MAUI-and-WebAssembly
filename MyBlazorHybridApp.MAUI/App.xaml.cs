1677207236081964ecaf763c5d098bde678cc978
jerryurena <jerryurenaa@gmail.com>
2024-12-29 16:01:16 -0500

    Initial commit

---
+++ MyBlazorHybridApp.MAUI/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace MyBlazorHybridApp.MAUI;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new MainPage()) { Title = "MyBlazorHybridApp.MAUI" };
+	}
+}
---
+++ MyBlazorHybridApp.MAUI/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MyBlazorHybridApp.MAUI;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
